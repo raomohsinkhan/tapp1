9434629a74255269d9301814139e1508c681bc39
Rao Mohsin <kraomohsin75@gmail.com>
2021-10-12 20:27:44 +0500

    ghgh

--- tempapp2/urls.py
+++ tempapp2/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('t1', views.t1,name='t1'),
     path('t2', views.t2,name='t2'),
     path('t3', views.t3,name='t3'),
+    #path('register',views.register,name='register')
 ]
--- tempapp2/views.py
+++ tempapp2/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 
@@ -8,8 +10,33 @@ def home(request):
 
 
 def t1(request):
-	return HttpResponse("Tempapp2: This is a t1 page.")
-
+	if request.method == 'POST':
+		# INSER DATA INTO DATABASE
+		fname = request.POST['fname']
+		lname = request.POST['lname']
+		username = request.POST['username']
+		password1 = request.POST['password1']
+		password2 = request.POST['password2']
+		email = request.POST['email']
+		if password1 == password2:
+			if User.objects.filter(username=username).exists():
+				messages.info(request,'username has been booked')
+				return redirect('t1')
+			elif User.objects.filter(email=email).exists():
+				messages.info(request,'Email has been booked soemone else, do not know')
+				return redirect('t1')
+			else:
+				user = User.objects.create_user(email=email,password=password1,first_name=fname,last_name=lname,username=username)
+				user.save()
+				messages.info(request,'user created successfully')
+				return redirect('/')
+			return redirect('/')
+		else:
+			messages.info(request,'Password not matched...')
+			return redirect('t1')
+		return redirect('/')
+	else:
+		return render(request,'register.html',{"title":"Register", "dests":"dests"})
 
 def t2(request):
 	return HttpResponse("Tempapp2: This is a t2 page.")
